52fa77c474ae35221c2cc57fb88a8f1b70c103db
Olumide Olubosede <olumideolubosede@gmail.com>
2025-01-20 12:16:47 +0100

    Improved

--- 2_Add_Two_Numbers.dart
+++ 2_Add_Two_Numbers.dart
@@ -1,4 +1,27 @@
 /**
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order, and each of their nodes contains a single digit. 
+ * Add the two numbers and return the sum as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * Example 2:
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * Constraints:
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have leading zeros.
+
+ * 
  * Definition for singly-linked list.
  * class ListNode {
  *   int val;
@@ -42,5 +65,26 @@ class Solution {
             curr = curr.next!;
         }
     }
+  }
+}
+
+// IMPROVED VERSION
+class Solution {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    ListNode dummyHead = ListNode(0);
+    ListNode curr = dummyHead;
+    int carry = 0;
+    while (l1 != null || l2 != null || carry != 0) {
+        int x = l1 != null ? l1.val : 0;
+        int y = l2 != null ? l2.val : 0;
+        int sum = carry + x + y;
+        carry = (sum / 10).floor();
+
+        curr.next = ListNode(sum % 10);
+        curr = curr.next!;
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
+    }
+    return dummyHead.next;
   }
 }
